7ea55020c6717976f7bf3c26c059f18de20facaf
Petr <p.y.klimenko@gmail.com>
2024-09-17 18:40:45 +0300

    upd

--- api/user/check-email.js
+++ api/user/check-email.js
@@ -1,4 +1,4 @@
-const { findUserByEmail } = require('../db/db-queries');
+const { findPersonByEmail } = require('../db/db-queries');
 const sendGmail = require('../google/google-auth');
 
 module.exports = async (req, res) => {
@@ -10,28 +10,24 @@ module.exports = async (req, res) => {
         const person = await findPersonByEmail(email);
         if (person) {
             console.log(`[check-email] Пользователь найден: ${person.firstName} ${person.lastName}`);
-            if (person instanceof Student) {
-                console.log(`[check-email] Найден студент с email: ${email}`);
-                res.status(200).json({ 
-                    type: 'student',
-                    ...person 
-                });
-            } else if (person instanceof Teacher) {
-                console.log(`[check-email] Найден преподаватель с email: ${email}`);
-                res.status(200).json({ 
-                    type: 'teacher',
-                    ...person 
-                });
-            }
+            
+            // Отправляем _id пользователя на клиент, чтобы потом использовать его для проверки
+            res.status(200).json({ 
+                _id: person._id,
+                email: person.email,
+                tgId: person.tgId
+            });
 
-            await sendGmail(person.email, 'Код регистрации в MARHIEduTrack', `Привет, ${person.firstName}. Чтобы завершить регистрацию, используй код ${person.tgId}.`);
-
+            // Отправляем код на почту
+            await sendGmail(person.email, 'Код регистрации в MARHIEduTrack', 
+                `Привет, ${person.firstName}. Чтобы завершить регистрацию, используй код ${person.tgId}.`
+            );
         } else {
             console.log(`[check-email] Пользователь с email: ${email} не найден`);
             res.status(404).json({ error: 'Пользователь не найден' });
         }
     } catch (error) {
-        console.error(`[find-by-tgId] Ошибка при поиске пользователя с email: ${email}`, error);
+        console.error(`[check-email] Ошибка при поиске пользователя с email: ${email}`, error);
         res.status(500).json({ error: 'Ошибка сервера' });
     }
 };
