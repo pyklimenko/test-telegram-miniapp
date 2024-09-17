5c364090ec900eab428b2740f3f49211d02b363b
Petr <p.y.klimenko@gmail.com>
2024-09-17 16:20:39 +0300

    upd

--- api/user/find-by-tgId.js
+++ api/user/find-by-tgId.js
@@ -3,25 +3,31 @@ const { findPersonByTgId } = require('../db/db-queries');
 module.exports = async (req, res) => {
     const { tgId } = req.query;
 
+    console.log(`[find-by-tgId] Получен запрос на поиск пользователя с tgId: ${tgId}`);
+
     try {
         const person = await findPersonByTgId(tgId);
         if (person) {
+            console.log(`[find-by-tgId] Пользователь найден: ${person.firstName} ${person.lastName}`);
             if (person instanceof Student) {
+                console.log(`[find-by-tgId] Найден студент с tgId: ${tgId}`);
                 res.status(200).json({ 
                     type: 'student',
                     ...person 
                 });
             } else if (person instanceof Teacher) {
+                console.log(`[find-by-tgId] Найден преподаватель с tgId: ${tgId}`);
                 res.status(200).json({ 
                     type: 'teacher',
                     ...person 
                 });
             }
         } else {
+            console.log(`[find-by-tgId] Пользователь с tgId: ${tgId} не найден`);
             res.status(404).json({ error: 'Пользователь не найден' });
         }
     } catch (error) {
-        console.error('Ошибка при поиске пользователя в базе:', error);
+        console.error(`[find-by-tgId] Ошибка при поиске пользователя с tgId: ${tgId}`, error);
         res.status(500).json({ error: 'Ошибка сервера' });
     }
 };
