008e733187b55a6ac7d39fe2f02e96cd7f08b6d9
Пётр Клименко <p.y.klimenko@gmail.com>
2024-09-17 22:34:49 +0300

    upd

--- api/user/verify-code.js
+++ api/user/verify-code.js
@@ -1,4 +1,4 @@
-const { findPersonById, updatePersonTgId } = require('../db/db-queries');
+const { findPersonById, updatePersonTgId, Student, Teacher } = require('../db/db-queries'); // Импортируем классы
 
 module.exports = async (req, res) => {
     const { _id, code, tgUserId } = req.body;
@@ -6,29 +6,26 @@ module.exports = async (req, res) => {
     console.log(`[verify-code] Получен запрос на сравнение ${_id} с ${code}`);
 
     try {
-
         if (_id === code) {
             console.log(`[verify-code] Код верный, регистрация завершена для пользователя с _id: ${_id}`);
 
-            const person = findPersonById(_id);
+            const person = await findPersonById(_id); // Используем await для асинхронного вызова
             if (person) {
                 console.log(`[verify-code] Пользователь найден: ${person.firstName} ${person.lastName}`);
+                
                 if (person instanceof Student) {
-                    console.log(`[verify-code] Найден студент с tgId: ${tgId}`);
-                    updatePersonTgId(_id, tgUserId, 'Students')
-    
+                    console.log(`[verify-code] Найден студент с tgId: ${tgUserId}`);
+                    await updatePersonTgId(_id, tgUserId, 'Students'); // Используем tgUserId
                 } else if (person instanceof Teacher) {
-                    console.log(`[verify-code] Найден преподаватель с tgId: ${tgId}`);
-                    updatePersonTgId(_id, tgUserId, 'Teachers')
+                    console.log(`[verify-code] Найден преподаватель с tgId: ${tgUserId}`);
+                    await updatePersonTgId(_id, tgUserId, 'Teachers'); // Используем tgUserId
                 }
+                
+                res.status(200).json({ message: 'Регистрация завершена' });
             } else {
-                console.log(`[verify-code] Пользователь с tgId: ${tgId} не найден`);
+                console.log(`[verify-code] Пользователь с _id: ${_id} не найден`);
                 res.status(404).json({ error: 'Пользователь не найден' });
             }
-
-            updatePersonTgId(_id, )
-
-            res.status(200).json({ message: 'Регистрация завершена' });
         } else {
             console.log(`[verify-code] Неверный код для пользователя с _id: ${_id}`);
             res.status(400).json({ error: 'Неверный код' });
