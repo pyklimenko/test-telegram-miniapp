5340776d6fe9ee7f33359287b9e3ce6bb9482fec
Пётр Клименко <p.y.klimenko@gmail.com>
2024-09-18 15:17:32 +0300

    upa Разбираемся с почтой

--- api/google/google-auth.js
+++ api/google/google-auth.js
@@ -4,16 +4,27 @@ async function googleAuthorize() {
     const client_id = process.env.CLIENT_ID;
     const client_secret = process.env.CLIENT_SECRET;
     const redirect_uris = [process.env.REDIRECT_URIS];
+
+    console.log('Проверка переменных окружения:');
+    console.log('CLIENT_ID:', !!client_id);
+    console.log('CLIENT_SECRET:', !!client_secret);
+    console.log('REDIRECT_URIS:', !!redirect_uris[0]);
+
     const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
     const accessToken = process.env.GMAIL_ACCESS_TOKEN;
     const refreshToken = process.env.GMAIL_REFRESH_TOKEN;
+
+    console.log('Проверка токенов:');
+    console.log('Access Token:', !!accessToken);
+    console.log('Refresh Token:', !!refreshToken);
 
     if (accessToken && refreshToken) {
         oAuth2Client.setCredentials({
             access_token: accessToken,
             refresh_token: refreshToken,
         });
+        console.log('Токены успешно установлены.');
         return oAuth2Client;
     }
 
--- api/google/google-mail.js
+++ api/google/google-mail.js
@@ -3,7 +3,14 @@ const googleAuthorize = require('./google-auth');
 
 async function sendGmail(to, subject, message) {
     try {
+        console.log('Пытаемся отправить письмо...');
+        console.log(`Отправка на адрес: ${to}`);
+        console.log(`Тема письма: ${subject}`);
+        console.log('Текст сообщения:', message);
+
         const auth = await googleAuthorize();
+        console.log('Авторизация завершена.');
+
         const gmail = google.gmail({ version: 'v1', auth });
 
         const subjectBase64 = `=?UTF-8?B?${Buffer.from(subject).toString('base64')}?=`;
@@ -17,12 +24,14 @@ async function sendGmail(to, subject, message) {
             message,
         ].join('\n');
 
+        console.log('Письмо подготовлено, кодируем...');
         const encodedMessage = Buffer.from(email)
             .toString('base64')
             .replace(/\+/g, '-')
             .replace(/\//g, '_')
             .replace(/=+$/, '');
 
+        console.log('Отправляем письмо...');
         const result = await gmail.users.messages.send({
             userId: 'me',
             requestBody: {
@@ -30,12 +39,11 @@ async function sendGmail(to, subject, message) {
             },
         });
 
-        console.log(`Письмо отправлено: ${result.data.id}`);
+        console.log(`Письмо успешно отправлено: ${result.data.id}`);
     } catch (error) {
         console.error('Ошибка при отправке письма через Gmail:', error);
         throw error;
     }
 }
-
 
 module.exports = sendGmail;
